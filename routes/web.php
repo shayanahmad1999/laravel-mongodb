23e47b5bb542ec3c116275391edc9d23e9a51c18
shayanahmad1999 <shayanahmad235@gmail.com>
2025-07-21 17:09:43 +0500

    Configuring application routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use MongoDB\Client;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class,'index'])
+    ->middleware('auth')    
+    ->name('home');
 
 Route::get('/ping', function () {
     try {
@@ -18,5 +23,28 @@ Route::get('/ping', function () {
 });
 
 Auth::routes();
+
+Route::controller(PostController::class)
+    ->prefix('posts')
+    ->name('posts.')
+    ->group(function(){
+        Route::get('/','index')->name('index');
+        Route::get('/create','create')->name('create');
+        Route::post('/store','store')->name('store');
+        Route::get('/{post}/show','show')->name('show');
+        Route::get('/{post}/edit','edit')->name('edit');
+        Route::patch('/{post}/update','update')->name('update');
+        Route::delete('/{post}/destroy','destroy')->name('destroy');
+    });
+
+Route::get('/profiles/{user}', [ProfileController::class, 'index'])->name('profile.index');
+Route::get('/profiles/{user}/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+Route::patch('/profiles/{user}', [ProfileController::class, 'update'])->name('profile.update');
+
+Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comment.store');
+Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comment.destroy');
+
+Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('like.store');
+Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('like.destroy');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
